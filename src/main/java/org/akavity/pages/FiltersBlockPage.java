8058b6df3e3e5e3ebd103f425d30cf07baced291
Basil Akavity <vakavity@gmail.com>
2025-10-24 13:35:14 +0300

    locators have been updated

--- src/main/java/org/akavity/pages/FiltersBlockPage.java
+++ src/main/java/org/akavity/pages/FiltersBlockPage.java
@@ -8,14 +8,14 @@ import static com.codeborne.selenide.Selenide.$;
 public class FiltersBlockPage {
     private final SelenideElement titleField = $(By.cssSelector("[class='catalog-title']"));
     private final SelenideElement salesButton = $(By.xpath("//span[text()='РАСПРОЖАРА']/.."));
-    private final SelenideElement minPrice = $(By.cssSelector("div[class*='filter__price'] input[name*='start']"));
-    private final SelenideElement maxPrice = $(By.cssSelector("div[class*='filter__price'] input[name*='end']"));
-    private final SelenideElement priceReadyButton = $(By.xpath("//button[contains(@class,'filter-btn') and contains(.,'Готово')]"));
+    private final SelenideElement minPrice = $(By.xpath("//div[@data-testid='catalog-filter']//div[contains(@class,'filter__price')]//input[contains(@name,'start')]/.."));
+    private final SelenideElement maxPrice = $(By.cssSelector("div[data-testid='catalog-filter'] div[class*='filter__price'] input[name*='end']"));
+    private final SelenideElement priceReadyButton = $(By.xpath("//div[@data-testid='catalog-filter']//button[contains(.,'Готово')]"));
     private final SelenideElement sorterButton = $(By.xpath("//button[contains(@class,'sorter')]/.."));
     private final SelenideElement allFiltersButton = $(By.cssSelector("button[class*='filter__btn--all']"));
 
     public SelenideElement getButton(String name) {
-        return $(By.xpath("//button[contains(@class,'filter__btn') and contains(.,'" + name + "')]"));
+        return $(By.xpath("//div[@data-testid='catalog-filter']/button[contains(.,'" + name + "')]"));
     }
 
     public SelenideElement getRadioButton(String name) {
